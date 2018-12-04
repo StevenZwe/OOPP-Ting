b00a38285e19ae03bb894575759f929c3c4a0715
zweth <181132Z@mymail.nyp.edu.sg>
2018-12-04 16:35:46 +0800

    changed to the app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, validators, PasswordField
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! Hello World 3123982748784 Halksdkasjdh as asdhkahsda sdasdasdas'
+def default():
+    return render_template('home.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/planner')
+def planner():
+    return render_template('planner.html')
 
 
 if __name__ == '__main__':
