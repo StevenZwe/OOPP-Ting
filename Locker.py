d833198ba331e51a4d0f3a30290f3719d466a788
Erin <180174Q@mymail.nyp.edu.sg>
2018-12-11 16:56:27 +0800

    Merge branch 'master' of https://github.com/StevenZwe/Organizer-Ting
    # Conflicts:
    #	.idea/workspace.xml

--- Locker.py
+++ Locker.py
@@ -1,137 +1,89 @@
-from wtforms import Form, StringField, DateField, SelectField, SubmitField
-from wtforms import validators, ValidationError
+class Locker:
+    def __init__(self, adminno, date, location, size):
+        self.__id = ''
+        self.__adminno = adminno
+        self.__date = date
+        self.__location = location
+        self.__size = size
+
+    def get_id(self):
+        return self.__id
+
+    def set_id(self, id):
+        self.__id = id
+
+    def get_adminno(self):
+        return self.__adminno
+
+    def set_adminno(self, adminno):
+        self.__adminno = adminno
+
+    def get_date(self):
+        return self.__date
+
+    def set_date(self, date):
+        self.__date = date
+
+    def get_location(self):
+        return self.__location
+
+    def set_location(self, location):
+        self.__location = location
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, size):
+        self.__size = size
 
 
-class Rental(Form):
-    adminno = StringField('Admin No.', [validators.Length(min=7, max=7)])
-    date = DateField('Date', [validators.DataRequired("Please enter a date")])
-    location = SelectField('Locker Location', choice=[("blka", "Block A"), ("blkl", "Block L"), ("blks", "Block S"),
-                                                      ("blkk", "Block K"), ("blkb", "Block B")])
-    size = SelectField('Locker Size', choice=[("small", "Small"), ("medium", "Medium"), ("big", "Big")])
-    submit = SubmitField('Submit')
-
-class Check(Form):
-    dateav = DateField('Date')
-    locationav = SelectField('Locker Location', choice=[("blka", "Block A"), ("blkl", "Block L"), ("blks", "Block S"),
-                                                        ("blkk", "Block K"), ("blkb", "Block B")])
-    submit = SubmitField('Check Availability')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#class Rental(Form):
-#    def __init__(self, adminno, date, location, size, submit):
-#        self.__id = ''
-#        self.__adminno = adminno
-#        self.__date = date
-#        self.__location = location
-#        self.__size = size
-#        self.__submit = submit
-#
-#    def get_adminno(self):
-#        return self.__adminno
-#
-#    def get_date(self):
-#        return self.__date
-#
-#    def get_location(self):
-#        return self.__location
-#
-#    def get_size(self):
-#        return self.__size
-#
-#    def get_submit(self):
-#        return self.__submit
-#
-#    def set_adminno(self, adminno):
-#        self.__adminno = adminno
-#
-#    def set_date(self, date):
-#        self.__date = date
-#
-#    def set_location(self, location):
-#        self.__location = location
-#
-#    def set_size(self, size):
-#        self.__size = size
-#
-#    def set_submit(self, submit):
-#        self.__submit = submit
-#
-#
-#
-#
-#
-#class Check(Form):
-#    def __init__(self, dateav, locationav, submit):
-#        self.__dateav = dateav
+#class Check:
+#    def __init__(self, locationav, sizeav, lockerno):
 #        self.__locationav = locationav
-#        self.__submit = submit
-#
-#    def get_dateav(self):
-#        return self.__dateav
+#        self.__sizeav = sizeav
+#        self.__lockerno = lockerno
 #
 #    def get_locationav(self):
 #        return self.__locationav
 #
-#    def get_submit(self):
-#        return self.__submit
-#
 #    def set_locationav(self, locationav):
 #        self.__locationav = locationav
 #
-#    def set_dateav(self, dateav):
-#        self.__dateav = dateav
+#    def get_sizeav(self):
+#        return self.__sizeav
 #
-#    def set_submit(self, submit):
-#        self.__submit = submit
-
-
-
-
-
-
-
-
-
-
-#def storelocker():
- #   lockerobj = {}
-
-#def lockertxtfile():
- #   rental_file = open('storelocker.txt','a')
-  #  rental_file.close()
-
-
-
-
-
-
-
-
-#@app.route('/lockers', methods=("GET", "POST"))
-#def lockers():
-#    form =
-#    if request.method == "POST"
-#        username = resuest.form['']
-
-
-
-
-
+#    def set_sizeav(self, sizeav):
+#        self.__sizeav = sizeav
+#
+#    def get_lockerno(self):
+#        return self.__lockerno
+#
+#    def set_lockerno(self, lockerno):
+#        self.__lockerno = lockerno
+#
+#def availability():
+#    lockers = ['A01', 'A02', 'A03', 'L01', 'L02', 'L03', 'S01', 'S02', 'S03']
+#    locker1 = ['A01']
+#    locker2 = ['A02']
+#    locker3 = ['A03']
+#    locker4 = ['L01']
+#    locker5 = ['L02']
+#    locker6 = ['L03']
+#    locker7 = ['S01']
+#    locker8 = ['S02']
+#    locker9 = ['S03']
+#
+#    location = ['blka', 'blkl', 'blks']
+#    size = ['small', 'medium', 'big']
+#
+#    blka = ['A01', 'A02', 'A03']
+#    blkl = ['L01', 'L02', 'L03']
+#    blks = ['S01', 'S02', 'S03']
+#
+#
+#
+#
+#
+#class LockerRental:
+#    locker = []
+#    date = []
