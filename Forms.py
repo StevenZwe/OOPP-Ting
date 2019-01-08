f4063bdac3e80e906d90b25b7019e90eab67b981
Erin <180174Q@mymail.nyp.edu.sg>
2019-01-08 13:48:38 +0800

    1/8/19 ERIN

--- Forms.py
+++ Forms.py
@@ -1,4 +1,5 @@
 from wtforms import *
+import shelve
 
 
 class CheckAvailForm(Form):
@@ -9,9 +10,15 @@ class CheckAvailForm(Form):
 
 
 class LockerForm(Form):
-    adminno = StringField('Admin Number')
-    date = DateField('Date', format='%Y-%m-%d')
-    location = SelectField('Locker Location', choices=[("SIT", "School of Information Technology"),
+    #db_read = shelve.open("storage.db", "r")
+    #lockerNos = db_read['locker']
+    #dateData = form.date.data
+    #kv = lockerNos.dateData
+    adminno = StringField('Admin Number:* ', )
+    date = DateField('Date:* ', format='%Y-%m-%d')
+    location = SelectField('Locker Location: ', choices=[("SIT", "School of Information Technology"),
                                                        ("SBM", "School of Business Management"),
                                                        ("SCL", "School of Chemical and Life Sciences")])
-    size = SelectField('Locker Size', choices=[("small", "Small"), ("medium", "Medium"), ("big", "Big")])
+    size = SelectField('Locker Size: ', choices=[("small", "Small"), ("medium", "Medium"), ("big", "Big")])
+    lockerno = SelectField('Locker Number: ', choices=[("","")])
+
